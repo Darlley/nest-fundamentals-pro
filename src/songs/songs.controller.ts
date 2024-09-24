fd9280cadf3c447f1dbb395af04bfd9995ba3623
Darlley Brito <darlleybrito@gmail.com>
2024-09-24 09:14:40 -0400

    feat: pipe

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,34 +1,71 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SongsService } from './songs.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateSongDto } from './dto/create-song.dto';
 import { UpdateSongDto } from './dto/update-song.dto';
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
   constructor(private readonly songsService: SongsService) {}
 
+  /**
+   * Cria uma nova música.
+   * 
+   * @param createSongDto Os dados da música a ser criada.
+   */
   @Post()
   create(@Body() createSongDto: CreateSongDto) {
     return this.songsService.create(createSongDto);
   }
 
+  /**
+   * Lista todas as músicas.
+   */
   @Get()
   findAll() {
     return this.songsService.findAll();
   }
 
+  /**
+   * Busca uma música específica por ID.
+   * 
+   * @param id O ID da música a ser buscada.
+   * 
+   * Nota: O ParseIntPipe pode ser passado diretamente sem instanciar
+   * se não houver necessidade de customizar o erro da requisição.
+   */
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE })) id: number) {
     return this.songsService.findOne(+id);
   }
 
+  /**
+   * Atualiza uma música existente.
+   * 
+   * @param id O ID da música a ser atualizada.
+   * @param updateSongDto Os dados atualizados da música.
+   */
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSongDto: UpdateSongDto) {
+  update(@Param('id') id: number, @Body() updateSongDto: UpdateSongDto) {
     return this.songsService.update(+id, updateSongDto);
   }
 
+  /**
+   * Remove uma música.
+   * 
+   * @param id O ID da música a ser removida.
+   */
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.songsService.remove(+id);
   }
 }
--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSongDto } from './dto/create-song.dto';
 import { UpdateSongDto } from './dto/update-song.dto';
 
@@ -17,15 +17,20 @@ export class SongsService {
   }
 
   findAll() {
-    return this.songs;
+    try {
+      return this.songs;
+    } catch (e) {
+      console.log('Controller@findAll', e);
+      throw new HttpException('server error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findOne(id: number) {
-    return this.songs.find(song => song.id === id);
+    return this.songs.find((song) => song.id === id);
   }
 
   update(id: number, updateSongDto: UpdateSongDto) {
-    const index = this.songs.findIndex(song => song.id === id);
+    const index = this.songs.findIndex((song) => song.id === id);
     if (index !== -1) {
       this.songs[index] = { ...this.songs[index], ...updateSongDto };
       return this.songs[index];
@@ -34,7 +39,7 @@ export class SongsService {
   }
 
   remove(id: number) {
-    const index = this.songs.findIndex(song => song.id === id);
+    const index = this.songs.findIndex((song) => song.id === id);
     if (index !== -1) {
       const removedSong = this.songs[index];
       this.songs.splice(index, 1);
